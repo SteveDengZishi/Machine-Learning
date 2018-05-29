5dcde809e7a2dfb084015c811ae5b398fdca791f
SteveDengZishi <zd475@nyu.edu>
2018-05-29 15:11:00 -0400

    imports

--- pandas_readData.py
+++ pandas_readData.py
@@ -7,6 +7,10 @@ Created on Mon May 28 13:14:49 2018
 """
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
+from sklearn.externals import joblib
 
 def data_to_html():
     # Read the dataset using pandas
@@ -24,6 +28,17 @@ def train_model():
     del df["unit_number"]
     del df["street_name"]
     del df["zip_code"]
+    
+    # Replace categorical data with one-hot encoded data
+    features_df = pd.get_dummies(df, columns=['garage_type','city'])
+    
+    # Remove the final sale price
+    del features_df['sales_price']
+    
+    # Create the X and Y arrays and convert them to numpy matrix
+    X = features_df.as_matrix()
+    Y = df['sales_price'].as_matrix()
+
 
 def main():
     
