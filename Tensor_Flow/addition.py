ec8c6f46edafdeb3b78a202bb7d9c7d11f0e43ca
SteveDengZishi <zd475@nyu.edu>
2018-08-27 02:49:15 -0400

    add

--- Tensor_Flow/addition.py
+++ Tensor_Flow/addition.py
@@ -9,8 +9,15 @@ Created on Mon Aug 27 02:19:24 2018
 import os
 import tensorflow as tf
 
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-#define tensors
+#define tensors and nodes
 X = tf.placeholder(tf.float32, name='X')
 Y = tf.placeholder(tf.float32, name='Y')
+
+addition = tf.add(X, Y, name='addition')
+
+#create session
+with tf.Session() as session:
+    result = session.run(addition, feed_dict={X:[1], Y:[4]})
+    print(result)
