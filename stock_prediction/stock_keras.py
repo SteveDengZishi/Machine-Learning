c4fd06d11780e8035dd180e3f53ebd3a1845540b
SteveDengZishi <zd475@nyu.edu>
2018-10-28 12:18:12 -0400

    changes

--- stock_prediction/stock_keras.py
+++ stock_prediction/stock_keras.py
@@ -5,15 +5,24 @@ Created on Fri Oct 19 13:28:06 2018
 
 @author: stevedeng
 """
+import datetime as dt
 import pandas as pd
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style 
 
-def data_to_html():
-    #Read the dataset using pandas
-    data_table = pd.read_csv("data_stocks.csv")
-    html = data_table[:100].to_html()
-    
-    #with open do not need a close statement
-    with open("data.html", "w") as outfile:
-        outfile.write(html)
+df = pd.read_csv('JD.csv', parse_dates=True, index_col=1)
+del df['Unnamed: 0']
+print(df.head(10))
+#Date is a datetime index
+style.use('ggplot')
+price_df = df.drop('Volume', axis=1)
+price_df['100MA'] = price_df['Adj Close'].rolling(window=100).mean()
+price_df['30MA'] = price_df['Adj Close'].rolling(window=30).mean()
+ax1 = plt.subplot2grid((6,1),(0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1),(5,0), rowspan=1, colspan=1, sharex=ax1)
 
-data_to_html()
+ax1.plot(price_df.index, price_df['Adj Close'])
+ax1.plot(price_df.index, price_df[['100MA','30MA']])
+ax2.bar(df.index, df['Volume'])
+plt.show()
